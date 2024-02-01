8e3b7c700171e9ee91e5cf087a3b07aa70fbffbb
Simon Gabay <gabay.simon@gmail.com>
2024-02-01 18:50:46 +0100

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,5 +9,5 @@ cd ..
 mv FONDUE-LA-PRINT-16/data/* train_data/fondue
 mv FONDUE-FR-PRINT-16/data/* train_data/fondue
 mv Lambertus/data/* train_data/new
-rm -rf FoNDUE-HTR/FONDUE-LA-PRINT-16
-rm -rf FoNDUE-HTR/FONDUE-FR-PRINT-16
+rm -rf FONDUE-LA-PRINT-16
+rm -rf FONDUE-FR-PRINT-16
