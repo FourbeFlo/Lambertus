fbaf2c3b9e5163e39c7a0ef74913f4fa4b4f97ba
Simon Gabay <gabay.simon@gmail.com>
2024-02-01 19:00:05 +0100

    Update sub_script_debug.sh

--- script/sub_script_debug.sh
+++ script/sub_script_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 #SBATCH --partition=debug-gpu
-#SBATCH --time=08:00:00
+#SBATCH --time=00:05:00
 #SBATCH --gpus=2
 #SBATCH --output=kraken-%j.out
 #SBATCH --mem=24GB
@@ -9,6 +9,9 @@
 
 module load fosscuda/2020b Python/3.8.6
 source ~/kraken-env/bin/activate
+wget https://github.com/Gallicorpora/Segmentation-and-HTR-Models/releases/download/1.0.0/Gallicorpora+_best.mlmodel
 
 echo "KETOS training"
-srun ketos train -f alto -i Train/Gallicorpora+_best.mlmodel --resize add -f alto -d cuda:0 -r 0.0001 --lag 10  --workers 12 --normalization NFD Train/data/*/*.xml
+srun ketos train -f alto -i Gallicorpora+_best.mlmodel --resize add -f alto -d cuda:0 -r 0.0001 --lag 10  --workers 12 --normalization NFD train_data/*/*/*.xml
+
+rm Gallicorpora+_best.mlmodel
