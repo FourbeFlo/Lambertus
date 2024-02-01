7d3d68e7e5419b8f0bf77c92971e44bbaba9a0ae
FourbeFlo <floriane.goyus@gmail.com>
2024-02-01 17:45:51 +0100

    Create OCRtrain_01.sh

---
+++ script/OCRtrain_01.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+#SBATCH --partition=debug-gpu
+#SBATCH --time=08:00:00
+#SBATCH --gpus=2
+#SBATCH --output=kraken-%j.out
+#SBATCH --mem=24GN
+#SBATCH --ntasks=12
+#SBATCH --gres=gpu:2,VramPerGpu:24GB
+
+module load fosscuda/2020b Python/3.8.6
+
+echo "KETOS training"
+srun ketos train -f alto -i Gallicorpora+_best.mlmodel --resize add -f alto -d cuda:0 -r 0.0001 --lag 10  --workers 12 --normalization NFD data/*/*.xml
